d2af537ac693286b6156043cf00cd2ef0351d122
eshavignesh123 <94078775+eshavignesh123@users.noreply.github.com>
2024-02-04 21:14:59 -0500

    Update sig.py

--- sig.py
+++ sig.py
@@ -2,25 +2,9 @@ import streamlit as st
 
 import cv2 
 
+st.title("Signature Modification")
+
+
 file_name = st.file_uploader("Upload file", type=["csv", "png", "jpg"])
   
-
-
-src = cv2.imread(file_name, 1) 
-
-tmp = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY) 
-  
-alpha = cv2.threshold(tmp, 100, 255, cv2.THRESH_BINARY_INV) 
-
-b, g, r = cv2.split(src) 
-
-rgba = [b, g, r, alpha] 
-
-dst = cv2.merge(rgba, 4) 
-
-cv2.imwrite("signature_white.png", dst) 
-
-
-st.title("Signature Modification")
-st.image("signature_white.png")
 
